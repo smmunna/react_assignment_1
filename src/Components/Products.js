762b5e3a88359b9284a55f4c22887960facd0774
Minhazul Abedin Munna <minhazulabedinmunna@gmail.com>
2022-10-21 21:47:29 -0700

    update without design

---
+++ src/Components/Products.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+function Products(props) {
+  const { title, price, image, description, rating } = props;
+  return <div>
+      
+      <img src={image} class="card-img-top" alt="..." />
+      <div class="card-body">
+        <h5 class="card-title">{title}</h5>
+        <p className="card-text">Price: ${price}</p>
+        <p className="card-text">Rating: {rating}/5</p>
+        <p class="card-text">{description}</p>
+        <button className="btn btn-primary">Add to Card</button>
+      </div>
+      </div>
+    
+}
+
+export default Products;
